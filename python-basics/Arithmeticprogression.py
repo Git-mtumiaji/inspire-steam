f0abeb0aa0d724da153eae223779f75ad8920688
Ronnie Gichohi <gichohi.ronnie@gmail.com>
2026-02-15 10:46:46 -0800

    loops,maths funcctions, arithmetic progressions, Geometric progressions and string formation

--- python-basics/Arithmeticprogression.py
+++ python-basics/Arithmeticprogression.py
@@ -13,6 +13,13 @@ sn = (n / 2) * ((2 * a) + ((n - 1) * d))
 print(f"The nth term is : {nth_term}")
 print(f"The sum of the terms is : {sn}")
 
+# program to calculate geometric progression
+
+# Assignment 
+
+a = int(input("Enter the first number :"))
+n = int(input("Enter the number of terms :"))
 r = int(input("Enter the common ratio :"))
+
 sn_gp = (a * (r**n)) / (r - 1)
 print(f"The sum of the terms is : {sn_gp}")
